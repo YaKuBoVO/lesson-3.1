e3b8fd9ccc7a5b7a823087cfab9d83a8b18dd95e
YaKuBoVO <oyakubov526@gmail.com>
2023-05-04 21:24:40 +0500

    pashti tayyor

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-document.querySelector('.themetoggle').addEventListener('click', (event) => {
-   event.preventDefault();
-   if (localStorage.getItem('theme') === 'dark') {
-      localStorage.removeItem('theme');
-   }
-   else {
-      localStorage.setItem('theme', 'dark')
-   }
-   addDarkClassToHTML()
-});
+//document.querySelector('.themetoggle').addEventListener('click', (event) => {
+//   event.preventDefault();
+//   if (localStorage.getItem('theme') === 'dark') {
+//      localStorage.removeItem('theme');
+//   }
+//   else {
+//      localStorage.setItem('theme', 'dark')
+//   }
+//   addDarkClassToHTML()
+//});
 
-function addDarkClassTOHTML() {
-   try {
-      if (localStorage.getItem('theme') === 'dark') {
-         document.querySelector('html').classList.add('dark');
-         document.querySelector('.themetoggle i').textContent = 'dark_mode';
-      }
-      else {
-         document.querySelector('html').classList.remove('dark');
-         document.querySelector('.themetoggle i').textContent = 'wb_sunny';
-      }
-   } catch (err) { }
-}
+//function addDarkClassTOHTML() {
+//   try {
+//      if (localStorage.getItem('theme') === 'dark') {
+//         document.querySelector('html').classList.add('dark');
+//         document.querySelector('.themetoggle i').textContent = 'dark_mode';
+//      }
+//      else {
+//         document.querySelector('html').classList.remove('dark');
+//         document.querySelector('.themetoggle i').textContent = 'bx-moon';
+//      }
+//   } catch (err) { }
+//}
 
-addDarkClassToHTML();
+//addDarkClassToHTML();
